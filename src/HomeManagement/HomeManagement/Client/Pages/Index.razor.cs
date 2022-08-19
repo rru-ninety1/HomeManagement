c29b1118cbd58f2de767b080b6752821fe71a370
Remo Rusconi <remo.rusconi@espritsrl.it>
2022-08-19 15:13:51 +0200

    Remove test button

--- src/HomeManagement/HomeManagement/Client/Pages/Index.razor.cs
+++ src/HomeManagement/HomeManagement/Client/Pages/Index.razor.cs
@@ -1,23 +1,6 @@
-﻿using Fluxor;
-using HomeManagement.Client.Features.Catalog.ProductCategories.EditProductCategory.Store;
-using Microsoft.AspNetCore.Components;
-
-namespace HomeManagement.Client.Pages
+﻿namespace HomeManagement.Client.Pages
 {
     public partial class Index
     {
-        [Inject]
-        public IDispatcher Dispatcher { get; set; }
-
-
-        public void EndEditCategory()
-        {
-            Dispatcher.Dispatch(new EditProductCategoryEndEditProductCategoryAction());
-        }
-
-        public void SaveCategory()
-        {
-            Dispatcher.Dispatch(new EditProductCategorySaveProductCategoryAction(new Features.Catalog.ProductCategories.Models.ProductCategory { Id = "1", Description = "descr1" }, false));
-        }
     }
 }
