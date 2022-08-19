604a1a8d1447f8976447ea65b90e0612e55557f7
Remo Rusconi <remo.rusconi@espritsrl.it>
2022-08-19 17:55:16 +0200

    Synchronizer store

---
+++ src/HomeManagement/HomeManagement/Client/Features/Synchronizer/Store/SynchronizerReducers.cs
@@ -0,0 +1,16 @@
+﻿using Fluxor;
+
+namespace HomeManagement.Client.Features.Synchronizer.Store
+{
+    public static class SynchronizerReducers
+    {
+        [ReducerMethod(typeof(SynchronizerStartAction))]
+        public static SynchronizerState OnStart(SynchronizerState state) => state with { OnSync = true };
+
+        [ReducerMethod(typeof(SynchronizerEndAction))]
+        public static SynchronizerState OnEnd(SynchronizerState state) => state with { OnSync = false, OperationDescription = "" };
+
+        [ReducerMethod]
+        public static SynchronizerState OnSetOperationDescription(SynchronizerState state, SynchronizerSetOperationDescriptionAction action) => state with { OperationDescription = action.OperationDescription };
+    }
+}
---
+++ src/HomeManagement/HomeManagement/Client/Features/Synchronizer/Store/SynchronizerState.cs
@@ -0,0 +1,13 @@
+﻿using Fluxor;
+
+namespace HomeManagement.Client.Features.Synchronizer.Store
+{
+    [FeatureState(Name = "SynchronizerState")]
+    public record class SynchronizerState
+    {
+        public bool OnSync { get; init; }
+        public string OperationDescription { get; init; }
+
+        public SynchronizerState() => (OnSync, OperationDescription) = (false, "");
+    }
+}
