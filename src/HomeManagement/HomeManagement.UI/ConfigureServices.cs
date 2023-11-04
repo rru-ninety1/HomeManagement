d391b36aa701febfa0745da16b02173537959b7d
Remo Rusconi <remo.rusconi@espritsrl.it>
2023-11-04 15:09:18 +0100

    Initial culture

---
+++ src/HomeManagement/HomeManagement.UI/ConfigureServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeManagement.UI;
+public static class ConfigureServices
+{
+    public static IServiceCollection AddAppUIServices(this IServiceCollection services)
+    {
+        services.AddLocalization();
+
+        return services;
+    }
+}
